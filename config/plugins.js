e5f085289e76cb2557b592c6a636b8128d6877ca
Guilherme Augusto Raduenz <guilherme@trulogic.com.br>
2021-05-06 00:10:14 -0300

    upload changed to aws s3 and new types

---
+++ config/plugins.js
@@ -0,0 +1,15 @@
+module.exports = ({ env }) => ({
+  upload: {
+    provider: 'aws-s3',
+    providerOptions: {
+      accessKeyId: env('AWS_ACCESS_KEY_ID'),
+      secretAccessKey: env('AWS_ACCESS_SECRET'),
+      region: env('AWS_REGION'),
+      params: {
+        Bucket: env('AWS_BUCKET'),
+        // StorageClass: env('AWS_S3_STORAGE_CLASSES')
+      },
+      logger: console
+    }
+  }
+ });
